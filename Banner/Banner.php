ecd43e7c9f435a8a413b932a53952015780b9ac9
Yashar666 <yasar666@mail.ru>
2022-11-20 12:34:10 +0400

    add showimage to banner

--- Banner/Banner.php
+++ Banner/Banner.php
@@ -13,6 +13,7 @@ class Banner {
 	public function __construct(array $providers)
 	{
 		$this->setProviders($providers);
+		$this->showImage();
 	}
 
 	private function setProviders(array $providers): void
@@ -21,6 +22,16 @@ class Banner {
 			foreach ($providers as $provider) {
 				$this->{strtolower($provider->name)} = new $provider->value;
 			}
+		}
+	}
+
+	protected function showImage(): void
+	{
+		$image = Helper::realPath($this->config->get('IMG_URL'));
+		$imageInfo = getimagesize($image);
+		if($imageInfo && !empty($imageInfo['bits']) && Helper::isImage($imageInfo['mime'])) {
+			header("Content-type: {$imageInfo['mime']}");
+			readfile($image);
 		}
 	}
 }
--- Banner/Helper.php
+++ Banner/Helper.php
@@ -9,10 +9,24 @@ namespace Banner;
 use Exception;
 
 class Helper {
+
+	private static array $imgTypes = [
+		'image/png',
+		'image/jpeg',
+		'image/jpg',
+		'image/bmp',
+		'image/webp',
+		'image/gif',
+	];
 	
 	public static function realPath(string $path): string
 	{
 		return realpath(str_replace('~', getenv('DOCUMENT_ROOT'), $path));
+	}
+
+	public static function isImage(string $mime): bool
+	{
+		return in_array($mime, static::$imgTypes);
 	}
 
 	public static function readFile(string $file)
